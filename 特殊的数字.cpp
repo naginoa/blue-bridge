db3522452736435226aa7fb6baedb579ab201b7a
任欣源 <245735349@qq.com>
2018-03-03 16:19:38 +0800

    特殊的数字

---
+++ 特殊的数字.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	for(int a=1;a<=9;a++)
+	{
+		for(int b=0;b<=9;b++)
+		{
+			for(int c=0;c<=9;c++)
+			{
+				if(a*100 + b*10 + c == a*a*a + b*b*b + c*c*c)
+				{	
+					cout<<a<<b<<c<<endl;
+				}
+			}
+		}
+	}
+}
