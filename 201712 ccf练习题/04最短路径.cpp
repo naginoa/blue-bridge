41f329fc67b669bcf3782943303445c506612e7f
任欣源 <245735349@qq.com>
2018-03-10 20:04:26 +0800

    最短路径变种
    看着有思路
    用dfs，深度优先搜索算法实现，但是实际上代码遇到了问题，是思路中实现不了的。可能不可以用dfs，失败版

---
+++ 201712 ccf练习题/04最短路径.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define para 100000
+
+int dfs(int i, int j, int road[110][110][2], int n)
+{
+    if(road[i][j][0]!=0 || road[i][j][1]!= 0)
+    {
+        if(j==n)
+        {
+            return 
+        }
+    }
+}
+int main()
+{
+    int n,m;
+    int t,a,b,c;
+    int sum = 0;
+    int road[110][110][2]={0};
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        road[a][b][t] = c;
+    }
+    cin>>t>>a>>b>>c;
+    cout<<11;
+
+}
